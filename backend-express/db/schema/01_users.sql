13d52b09b3fcf60de17bd7e639032cf7a1c7c342
Shree Devi Yadam <shree.yadam@gmail.com>
2021-07-30 16:59:59 -0400

    Add rating to user schema

--- backend-express/db/schema/01_users.sql
+++ backend-express/db/schema/01_users.sql
@@ -8,5 +8,6 @@ CREATE TABLE users(
   password VARCHAR(50) NOT NULL,
   phone_number VARCHAR(32) NOT NULL,
   img_url VARCHAR(255),
-  is_provider BOOLEAN DEFAULT false
+  is_provider BOOLEAN DEFAULT false,
+  avg_rating INTEGER
 );
