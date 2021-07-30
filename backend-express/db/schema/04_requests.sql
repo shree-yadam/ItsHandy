316d8720163cea9231cea17bf338d4924092cf56
Shree Devi Yadam <shree.yadam@gmail.com>
2021-07-30 15:51:13 -0400

    Add reviews schema and update requests and messages schema

--- backend-express/db/schema/04_requests.sql
+++ backend-express/db/schema/04_requests.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS requests CASCADE;
 CREATE TABLE requests (
   id SERIAL PRIMARY KEY NOT NULL,
   title VARCHAR(50),
+  street_address VARCHAR(50),
   city VARCHAR(15),
   category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
   date_needed date,
@@ -11,5 +12,7 @@ CREATE TABLE requests (
   client_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   provider_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   date_completed date,
+  longitude DOUBLE PRECISION,
+  latitude DOUBLE PRECISION,
   price VARCHAR(15)
 );
