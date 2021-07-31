713c79e6fae6612d1ac4bef6d6fa884745779f24
Shree Devi Yadam <shree.yadam@gmail.com>
2021-07-31 15:18:31 -0400

    Add basic structure for components: NewjobListItem and RequestItemInfo

---
+++ client/src/components/NewJobListItem.jsx
@@ -0,0 +1,36 @@
+import RequestItemInfo from "./RequestItemInfo";
+import Button from "./Button";
+
+export default function NewJobListItem({
+  title,
+description,
+category,
+date
+}) {
+  const [quote, setQuote] = useState("");
+
+  function handleOffer(event) {
+    /* TBD */
+  }
+
+  function goToDetails(event) {
+    /* TBD */
+  }
+
+  return (
+    <div>
+
+      <RequestItemInfo title={title} description={description} category={category} date={date} />
+
+      <label for="quote">Quote:</label>
+      <input name="quote" value={quote} onChange={(event) => setQuote(event.target.value)} />
+      <Button variant="primary" type="submit"  onClick={handleOffer}>
+          Make a quick Offer
+        </Button>
+
+        <Button variant="primary" type="submit"  onClick={goToDetails}>
+         Details
+        </Button>
+    </div>
+  );
+}
---
+++ client/src/components/RequestItemInfo.jsx
@@ -0,0 +1,17 @@
+export default function RequestItemInfo({
+  title,
+  description,
+  category,
+  date
+}) {
+  return (
+    <div>
+      <div>
+      <h2>Title: <strong>{title}</strong></h4>
+      <p>Description: {description}</p>
+      <p>Category: {category}</p>
+      <p>Date needed: {date}</p>
+      </div>
+    </div>
+  );
+}
