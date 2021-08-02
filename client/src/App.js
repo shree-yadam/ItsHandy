1b4109019f03a6e09eadb31172baf28f2c3b67ac
Abanoub Shenouda <Abanoub Shenouda>
2021-08-02 12:52:21 -0400

    Fix merge conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,9 @@
 import "./App.css";
 import React, { useState } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 // App components
-import Home from "./components/Home"
+import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import NavBar from "./components/NavBar";
@@ -17,11 +12,10 @@ import Dashboard from "./components/Dashboard";
 import RequestForm from "./components/RequestForm";
 import NewJobList from "./components/NewJobList";
 import AssignedJobList from "./components/AssignedJobsList";
-import ProviderDashboard from "./components/ProviderDashboard";
-
+//import ProviderDashboard from "./components/ProviderDashboard";
 
 function App() {
-    //const [user, setUser] = createContext();
+  //const [user, setUser] = createContext();
   // const [currentUserDetails, setCurrentUSerDetails] = set
   // const currentUser =useContext(null);
 
@@ -34,53 +28,59 @@ function App() {
   const [currentUser, setCurrentUser] = useState(null);
 
   return (
-      <div className="App">
-    <Router>
-        <NavBar currentUser={currentUser} setCurrentUser={setCurrentUser}></NavBar>
-<main>
-        <Switch>
-        <Route path="/" exact >
-            <Home />
-          </Route>
+    <div className="App">
+      <Router>
+        <NavBar
+          currentUser={currentUser}
+          setCurrentUser={setCurrentUser}
+        ></NavBar>
+        <main>
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
 
-          <Route path="/login">
-            <Login currentUser={currentUser} setCurrentUser={setCurrentUser} />
-          </Route>
+            <Route path="/login">
+              <Login
+                currentUser={currentUser}
+                setCurrentUser={setCurrentUser}
+              />
+            </Route>
 
-          <Route path="/register">
-            <Register
-              currentUser={currentUser}
-              setCurrentUser={setCurrentUser}
-            />
-          </Route>
+            <Route path="/register">
+              <Register
+                currentUser={currentUser}
+                setCurrentUser={setCurrentUser}
+              />
+            </Route>
 
-          <Route path="/requests" currentUser={currentUser}>
+            <Route path="/requests" currentUser={currentUser}>
               <RequestList />
             </Route>
 
-          <Route exact path="/requests/new">
-            <RequestForm />
-          </Route>
+            <Route exact path="/requests/new">
+              <RequestForm />
+            </Route>
 
-          <Route path="/dashboard">
-            <Dashboard currentUser={currentUser} />
-          </Route>
+            <Route path="/dashboard">
+              <Dashboard currentUser={currentUser} />
+            </Route>
 
-          <Route path="/listings">
-            <NewJobList currentUser={currentUser} />
-          </Route>
+            <Route path="/listings">
+              <NewJobList currentUser={currentUser} />
+            </Route>
 
-          <Route path="/assigned_jobs">
-            <AssignedJobList currentUser={currentUser} />
-          </Route>
+            <Route path="/assigned_jobs">
+              <AssignedJobList currentUser={currentUser} />
+            </Route>
 
-          <Route path="*">
-            <h2>404 NOT FOUND</h2>
-          </Route>
-        </Switch>
+            <Route path="*">
+              <h2>404 NOT FOUND</h2>
+            </Route>
+          </Switch>
         </main>
-    </Router>
-      </div>
+      </Router>
+    </div>
   );
 }
 
