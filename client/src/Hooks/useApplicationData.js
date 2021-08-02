068cc68c4dcfb91e54c77f712d014bd5fe813d58
Abanoub Shenouda <Abanoub Shenouda>
2021-08-01 22:57:38 -0400

    Add custom hook for userRequsts

---
+++ client/src/Hooks/useApplicationData.js
@@ -0,0 +1,13 @@
+// import axios from "axios";
+// import React, { useState, useEffect } from "react";
+
+// export default function useApplicationData() {
+//   // const [user, setUser] = useState(
+//   // first_name:
+
+//   // )
+
+//   const [requestsState, setRequestsState] = useState();
+
+//   useEffect(() => {});
+// }
