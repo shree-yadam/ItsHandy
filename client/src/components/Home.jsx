be89e6b3402d8816a04f1c02df662e4407d17271
afzaanhakim <afzaanhakim19@gmail.com>
2021-08-03 11:47:05 -0400

    removed request form link from home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,45 +1,29 @@
-import React from 'react';
-import NewJobList from './NewJobList';
-import { useHistory } from 'react-router-dom'
-import Button from 'react-bootstrap/Button';
-import './home.scss'
-
-
+import React from "react";
+import NewJobList from "./NewJobList";
+import { useHistory } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import "./home.scss";
 
 export default function Home(currentUser) {
-const history = useHistory()
-return (
-  <div className = "main-container">
-<div className = 'about'>
-<h1>About Us</h1>
+  const history = useHistory();
+  return (
+    <div className="main-container">
+      <div className="about">
+        <h1>About Us</h1>
 
-<h2> ItsHandy is a modern day app </h2>
-  <p> To help you find service providers such as plumbers, electricians, painters etc</p>
+        <h2> ItsHandy is a modern day app </h2>
+        <p>
+          {" "}
+          To help you find service providers such as plumbers, electricians,
+          painters etc
+        </p>
+      </div>
 
-</div>
+      <div className="info">
+        <div className="testimonials">
 
-<div className = "info">
-<div className = "new-jobs">
-<NewJobList ></NewJobList>
-</div>
-
-
-
-<div className = "request-job-btn">
-<Button onClick={()=> history.push('/requests/new')}> Request Form </Button>
-</div>
-
-</div>
-
-
-
-
-</div>
-
-
-
-
-)
-
-
+        </div>
+      </div>
+    </div>
+  );
 }
