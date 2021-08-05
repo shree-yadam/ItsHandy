ea9d6a2d87bf1c15fd5262e0a85a3ff92eb93878
Shree Devi Yadam <shree.yadam@gmail.com>
2021-08-05 13:17:12 -0400

    Delete request from RequestList state

--- client/src/hooks/useRequestListData.js
+++ client/src/hooks/useRequestListData.js
@@ -5,13 +5,13 @@ import { useParams } from "react-router-dom";
 /**
  * Handles requests data
  */
-export default function useRequstListData() {
+export default function useRequestListData() {
   const { userId } = useParams();
   // Id of user who made the requests
   //const clientId = 1;
   //console.log("User id from params in requests list data", userId);
   // State for requests List
-  const [requestListState, setRequstListState] = useState(
+  const [requestListState, setRequestListState] = useState(
     {
       requestList: [
         //   {
@@ -46,7 +46,7 @@ export default function useRequstListData() {
       axios.get(`http://localhost:3001/api/client/${userId}/requests/offers`),
     ])
       .then((all) => {
-        setRequstListState((prev) => {
+        setRequestListState((prev) => {
           return {
             requestList: all[0].data,
             offers: all[1].data,
@@ -56,5 +56,5 @@ export default function useRequstListData() {
       })
       .catch((err) => err.message);
   }, [userId]);
-  return { requestListState, setRequstListState };
+  return { requestListState, setRequestListState };
 }
