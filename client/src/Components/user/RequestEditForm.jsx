991beefa5cc9b97b06bcc5bde6a567830f9f932b
Shree Devi Yadam <shree.yadam@gmail.com>
2021-08-10 11:53:41 -0400

    Fix request edit form position and focus

--- client/src/Components/user/RequestEditForm.jsx
+++ client/src/Components/user/RequestEditForm.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import { Form } from "react-bootstrap";
 import "./RequestForm.scss";
@@ -101,8 +101,13 @@ export default function RequestEditForm({
 
   };
 
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, []);
+
+
   return (
-    <div className="form-container">
+    <div className="form-container request-edit">
       <h2> Edit Your Request </h2>
 
       <Form.Group className="mb-3" controlId="title">
